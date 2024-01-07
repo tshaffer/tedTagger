a3dae9f887a7b514228b06a8f25cefe4d1dfd420
Ted Shaffer <shaffer.family@gmail.com>
2024-01-07 07:08:21 -0800

    yet another checkpoint

--- src/selectors/keywords.ts
+++ src/selectors/keywords.ts
@@ -25,7 +25,6 @@ export function getKeywordsAsTree(tedTaggerState: TedTaggerState): any {
   const keywordRootNodeId: string = keywordsState.keywordRootNodeId;
   const keywordRootNode: KeywordNode | undefined = getNodeByNodeId(keywordsState, keywordRootNodeId);
   if (!isNil(keywordRootNode)) {
-    debugger;
     const childNodeIds: string[] = keywordRootNode.childrenNodeIds || [];
     const childNodes: KeywordNodeDeep[] = childNodeIds.map((childNodeId: string) => {
       const childNode: KeywordNode | undefined = getNodeByNodeId(keywordsState, childNodeId);
@@ -62,19 +61,36 @@ export function getKeywordsAsTree(tedTaggerState: TedTaggerState): any {
 
 function recursiveBuildTree(keywordsState: KeywordsState, deepKeywordTree: KeywordTreeDeep, parentNodeDeep: KeywordNodeDeep): void {
   if (parentNodeDeep.childNodeIds && parentNodeDeep.childNodeIds.length > 0) {
-    for (const childNodeId of parentNodeDeep.childNodeIds) {
-      const childNode: KeywordNode | undefined = getNodeByNodeId(keywordsState, childNodeId);
-      if (!isNil(childNode)) {
+    for (const childNode of parentNodeDeep.childNodes) {
 
-        const parentKeyword: Keyword = keywordsState.keywordsById[parentNodeDeep.keywordId];
-        console.log('Parent keyword: ', parentKeyword.label);
+      const parentKeyword: Keyword = keywordsState.keywordsById[parentNodeDeep.keywordId];
+      console.log('Parent keyword: ', parentKeyword.label);
 
-        const keyword: Keyword = keywordsState.keywordsById[childNode.keywordId];
-        console.log('Node keyword: ', keyword.label);
+      const keyword: Keyword = keywordsState.keywordsById[childNode.keywordId];
+      console.log('Node keyword: ', keyword.label);
 
+      // add full nodes to childNode
+      const childNodeIds: string[] = childNode.childNodeIds;
+      childNode.childNodes = childNodeIds.map((childNodeId: string) => {
+        const grandchildNode: KeywordNode | undefined = getNodeByNodeId(keywordsState, childNodeId);
+        if (!isNil(grandchildNode)) {
+          const childNodeDeep: KeywordNodeDeep = {
+            nodeId: grandchildNode.nodeId,
+            keywordId: grandchildNode.keywordId,
+            childNodeIds: grandchildNode.childrenNodeIds || [],
+            childNodes: [],
+          };
+          return childNodeDeep as KeywordNodeDeep;
+        } else {
+          debugger;
+          return {
+            nodeId: 'error',
+            keywordId: 'error',
+          } as KeywordNodeDeep;
+        }
+      });
+      recursiveBuildTree(keywordsState, deepKeywordTree, childNode);
 
-        recursiveBuildTree(keywordsState, deepKeywordTree, childNode);
-      }
     }
     // debugger;
   }
