94ad25b4f410e7bf43689fbda8198789cb1eabed
Ted Shaffer <shaffer.family@gmail.com>
2024-01-13 05:54:38 -0800

    MuiTreeView with checkbox. Includes test file but doesn't use it.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,6 +11,7 @@ import { addKeyword } from '../controllers';
 import { KeywordNode } from '../types';
 import { getKeywordRootNodeId } from '../selectors';
 import Keywords from './Keywords';
+import IconExpansionTreeView from './MuiTestTreeView';
 
 export interface AppProps {
   onLoadDefaultTagAvatarId: () => any;
@@ -54,56 +55,8 @@ const App = (props: AppProps) => {
       }, []);
   });
 
-  // React.useEffect(() => {
-  //   const handleKeyPress = (event: KeyboardEvent) => {
-  //     // if (event.key === 'Escape') {
-  //     //   // Handle the escape key press here
-  //     //   console.log('Escape key pressed!');
-  //     //   // Add your logic to handle the escape key press globally
-  //     // }
-  //     console.log('key pressed: ' + event.key);
-
-  //     if (event.key === 's') {
-  //       const samNode = props.onAddKeyword(props.keywordRootNodeId, 'Sam', 'person');
-  //       console.log('samNode: ');
-  //       console.log(samNode);
-  //     } else if (event.key === 'j') {
-  //       const joelNode = props.onAddKeyword(props.keywordRootNodeId, 'Joel', 'person');
-  //       console.log('joelNode: ');
-  //       console.log(joelNode);
-  //     } else if (event.key === 'r') {
-  //       const rachelNode = props.onAddKeyword(props.keywordRootNodeId, 'Rachel', 'person');
-  //       console.log('rachelNode: ');
-  //       console.log(rachelNode);
-  //     } else if (event.key === 'a') {
-  //       const grandmaEmilyNode: KeywordNode = props.onAddKeyword(props.keywordRootNodeId, 'Grandma Emily', 'person');
-  //       const tedNode: KeywordNode = props.onAddKeyword(grandmaEmilyNode.nodeId, 'Ted', 'person');
-  //       const noahNode: KeywordNode = props.onAddKeyword(grandmaEmilyNode.nodeId, 'Noah', 'person');
-  //       const samNode: KeywordNode = props.onAddKeyword(tedNode.nodeId, 'Sam', 'person');
-  //       const joelNode: KeywordNode = props.onAddKeyword(tedNode.nodeId, 'Joel', 'person');
-  //       const rachelNode: KeywordNode = props.onAddKeyword(tedNode.nodeId, 'Rachel', 'person');
-  //       const mattNode: KeywordNode = props.onAddKeyword(noahNode.nodeId, 'Matt', 'person');
-  //       const andrewNode: KeywordNode = props.onAddKeyword(noahNode.nodeId, 'Andrew', 'person');
-  //       const elenaNode: KeywordNode = props.onAddKeyword(mattNode.nodeId, 'Elena', 'person');
-  //       console.log('all nodes added');
-  //     }
-  //   };
-
-  //   // Add the event listener when the component mounts
-  //   console.log('addEventListener!');
-  //   document.addEventListener('keydown', handleKeyPress);
-
-  //   // Remove the event listener when the component unmounts
-  //   return () => {
-  //     console.log('removeEventListener!');
-  //     document.removeEventListener('keydown', handleKeyPress);
-  //   };
-  // }, []); // Empty dependency array ensures that the effect runs only once on mount
-
-
   // <LoupeView mediaItemId={'AEEKk92TFxiITyv1uvnEtu4aGKNUyEDUUMoy2rNoJ3HlErxsTjpi8wyK0-BJt3Uzly0ipMNrYrxnf1Xp57m40NlLF9bxUVpsSg'} />
   // <LoupeView mediaItemId={'AEEKk90Fx9zbfbE_1YBjDw6BrHlfnSWVtuYvPtcYmkWW8ZCUyL2QlqL2_krRkWMaTlA2gMTNx6eU0ob79Lqd_A9v9YYpXKyaow'} />
-  // <GridView />
 
   return (
     <div>
@@ -111,6 +64,7 @@ const App = (props: AppProps) => {
       <div className='appStyle'>
         <div className='leftColumnStyle'>
           <Keywords />
+          {/* <IconExpansionTreeView /> */}
         </div>
         <div className='centerColumnStyle'>
           <GridView />
--- src/components/Keywords.tsx
+++ src/components/Keywords.tsx
@@ -8,9 +8,99 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
+
+
+import clsx from 'clsx';
+import Typography from '@mui/material/Typography';
+import {
+  TreeItemProps,
+  useTreeItem,
+  TreeItemContentProps,
+} from '@mui/x-tree-view/TreeItem';
+import Checkbox from '@mui/material/Checkbox';
+
+
+
+
 import { Keyword, KeywordNodeDeep, KeywordTreeDeep, StringToKeywordLUT } from '../types';
 import { TedTaggerDispatch } from '../models';
 import { getAppInitialized, getKeywordRootNodeId, getKeywordsAsTree, getKeywordsById } from '../selectors';
+
+const CustomContent = React.forwardRef(function CustomContent(
+  props: TreeItemContentProps,
+  ref,
+) {
+  const {
+    classes,
+    className,
+    label,
+    nodeId,
+    icon: iconProp,
+    expansionIcon,
+    displayIcon,
+  } = props;
+
+  const {
+    disabled,
+    expanded,
+    selected,
+    focused,
+    handleExpansion,
+    handleSelection,
+    preventSelection,
+  } = useTreeItem(nodeId);
+
+  const icon = iconProp || expansionIcon || displayIcon;
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    preventSelection(event);
+  };
+
+  const handleExpansionClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    handleExpansion(event);
+  };
+
+  const handleSelectionClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    handleSelection(event);
+  };
+
+  return (
+    <div
+      className={clsx(className, classes.root, {
+        [classes.expanded]: expanded,
+        [classes.selected]: selected,
+        [classes.focused]: focused,
+        [classes.disabled]: disabled,
+      })}
+      onMouseDown={handleMouseDown}
+      ref={ref as React.Ref<HTMLDivElement>}
+    >
+      <div onClick={handleExpansionClick} className={classes.iconContainer}>
+        {icon}
+      </div>
+      <Typography
+        onClick={handleSelectionClick}
+        component="div"
+        className={classes.label}
+      >
+        {label}
+      </Typography>
+      <Checkbox />
+    </div>
+  );
+});
+
+const CustomTreeItem = React.forwardRef(function CustomTreeItem(
+  props: TreeItemProps,
+  ref: React.Ref<HTMLLIElement>,
+) {
+  return <TreeItem ContentComponent={CustomContent} {...props} ref={ref} />;
+});
+
 
 export interface KeywordsProps {
   appInitialized: boolean;
@@ -31,7 +121,7 @@ const Keywords = (props: KeywordsProps) => {
       const keyword: Keyword = props.keywordsById[keywordNode.keywordId];
       const keywordLabel: string = keyword.label;
       return (
-        <TreeItem
+        <CustomTreeItem
           key={keywordNode.nodeId}
           nodeId={keywordNode.nodeId}
           label={keywordLabel}
@@ -43,13 +133,13 @@ const Keywords = (props: KeywordsProps) => {
     });
 
     return (
-      <TreeItem
+      <CustomTreeItem
         key={keywordNode.nodeId}
         nodeId={keywordNode.nodeId}
         label={props.keywordsById[keywordNode.keywordId].label}
       >
         {keywordNodes}
-      </TreeItem>
+      </CustomTreeItem>
     );
   };
 
@@ -89,3 +179,4 @@ const mapDispatchToProps = (dispatch: TedTaggerDispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Keywords);
+
