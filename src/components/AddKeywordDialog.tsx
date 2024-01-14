7cbb81fb900d815a940f05e3689f831e888653dc
Ted Shaffer <shaffer.family@gmail.com>
2024-01-14 06:13:51 -0800

    keyword dialog wip

--- src/components/AddKeywordDialog.tsx
+++ src/components/AddKeywordDialog.tsx
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button, DialogActions, DialogContent } from '@mui/material';
 
 export interface AddKeywordDialogPropsFromParent {
   open: boolean;
@@ -14,26 +17,56 @@ export interface AddKeywordDialogProps extends AddKeywordDialogPropsFromParent {
 
 function AddKeywordDialog(props: AddKeywordDialogProps) {
 
+  const [keywordLabel, setKeywordLabel] = React.useState('');
+
   const { open, onClose } = props;
 
   const handleClose = () => {
     onClose();
   };
 
+  function handleAddNewKeyword(): void {
+    console.log('handleAddNewKeyword');
+  }
+
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>AddKeyword MealWheel</DialogTitle>
-      <div style={{ marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
-        pizza
-      </div>
-      <div
-        style={{
-          position: 'absolute',
-          bottom: '10px',
-          right: '10px',
-        }}
+      <DialogContent>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div style={{ marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
+            <TextField
+              label="Keyword Label"
+              value={keywordLabel}
+              onChange={(event) => setKeywordLabel(event.target.value)}
+            />
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '10px',
+              right: '10px',
+            }}
+          >
+          </div>
+        </Box>
+      </DialogContent>
+      <DialogActions
+        style={{ marginTop: '10px' }}
       >
-      </div>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleAddNewKeyword} autoFocus>
+          Add
+        </Button>
+      </DialogActions>
+
     </Dialog>
   );
 }
