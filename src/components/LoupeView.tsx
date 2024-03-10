5426a277e47b67467b94e39d00073a0d2064fd1a
Ted Shaffer <shaffer.family@gmail.com>
2024-03-10 07:55:00 -0700

    First attempt to support navigating in loupe view mode - not working so well.

--- src/components/LoupeView.tsx
+++ src/components/LoupeView.tsx
@@ -3,8 +3,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import '../styles/TedTagger.css';
-import { TedTaggerDispatch } from '../models';
-import { getMediaItemById } from '../selectors';
+import { TedTaggerDispatch, setLoupeViewMediaItemIdRedux } from '../models';
+import { getMediaItemById, getMediaItems } from '../selectors';
 import { MediaItem } from '../types';
 import { getPhotoUrl } from '../utilities';
 import { isNil } from 'lodash';
@@ -15,11 +15,69 @@ export interface LoupeViewPropsFromParent {
 
 export interface LoupeViewProps extends LoupeViewPropsFromParent {
   mediaItem: MediaItem | null;
+  mediaItems: MediaItem[];
+  onSetLoupeViewMediaItemId: (id: string) => any;
 }
 
 const LoupeView = (props: LoupeViewProps) => {
 
+  React.useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      console.log('handleKeyPress: ' + event.key);
+
+      switch (event.key) {
+        case 'ArrowRight':
+          handleDisplayNextPhoto();
+          break;
+        case 'ArrowLeft':
+          handleDisplayPreviousPhoto();
+          break;
+        default:
+          break;
+      }
+    };
+
+    // Add the event listener when the component mounts
+    console.log('addEventListener!');
+    document.addEventListener('keydown', handleKeyPress);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      console.log('removeEventListener!');
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []); // Empty dependency array ensures that the effect runs only once on mount
+
+
   const [windowDimensions, setWindowDimensions] = React.useState(getWindowDimensions());
+
+  const handleDisplayPreviousPhoto = () => {
+    const mediaItemIndex = props.mediaItems.findIndex((mediaItem: MediaItem) => mediaItem.googleId === props.mediaItemId);
+    console.log('handleDisplayPrevPhoto: mediaItemIndex: ' + mediaItemIndex);
+    const previousMediaItemIndex = mediaItemIndex - 1;
+    if (previousMediaItemIndex < 0) {
+      console.log('at beginning');
+      return;
+    } else {
+      const previousMediaItem = props.mediaItems[previousMediaItemIndex];
+      console.log('previousMediaItem: ' + previousMediaItem);
+      props.onSetLoupeViewMediaItemId(previousMediaItem.googleId);
+    }
+  };
+
+  const handleDisplayNextPhoto = () => {
+    const mediaItemIndex = props.mediaItems.findIndex((mediaItem: MediaItem) => mediaItem.googleId === props.mediaItemId);
+    console.log('handleDisplayNextPhoto: mediaItemIndex: ' + mediaItemIndex);
+    const nextMediaItemIndex = mediaItemIndex + 1;
+    if (nextMediaItemIndex >= props.mediaItems.length) {
+      console.log('at end');
+      return;
+    } else {
+      const nextMediaItem = props.mediaItems[nextMediaItemIndex];
+      console.log('nextMediaItem: ' + nextMediaItem);
+      props.onSetLoupeViewMediaItemId(nextMediaItem.googleId);
+    }
+  };
 
   function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
@@ -62,11 +120,13 @@ const LoupeView = (props: LoupeViewProps) => {
 function mapStateToProps(state: any, ownProps: any) {
   return {
     mediaItem: getMediaItemById(state, ownProps.mediaItemId),
+    mediaItems: getMediaItems(state),
   };
 }
 
 const mapDispatchToProps = (dispatch: TedTaggerDispatch) => {
   return bindActionCreators({
+    onSetLoupeViewMediaItemId: setLoupeViewMediaItemIdRedux,
   }, dispatch);
 };
 
