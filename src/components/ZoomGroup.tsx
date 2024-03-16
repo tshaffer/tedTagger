c10add7c025677a1bbf9a1214396e735b33b63be
Ted Shaffer <shaffer.family@gmail.com>
2024-03-16 05:52:50 -0700

    added placeholders for click handlers

--- src/components/ZoomGroup.tsx
+++ src/components/ZoomGroup.tsx
@@ -1,49 +1,33 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
 import { FormControl } from '@mui/material';
-import { Label } from '@mui/icons-material';
-
-const buttons = [
-  <Button key="zoomIn">Zoom In</Button>,
-  <Button key="zoomOut">Zoom Out</Button>,
-];
 
 export default function ZoomGroup() {
-  // return (
-  //   <Box
-  //     sx={{
-  //       display: 'flex',
-  //       '& > *': {
-  //         m: 1,
-  //       },
-  //     }}
-  //   >
-  //     <ButtonGroup
-  //       orientation="vertical"
-  //       aria-label="Vertical button group"
-  //       variant="text"
-  //     >
-  //       {buttons}
-  //     </ButtonGroup>
-  //   </Box>
-  // );
+
+  function handleZoomIn(): void {
+    console.log('Zoom In');
+  }
+
+  function handleZoomOut(): void {
+    console.log('Zoom Out');
+  }
 
   return (
     <FormControl style={{ marginLeft: '0px' }}>
       <span style={{ marginTop: '6px', marginBottom: '6px', display: 'block' }}>Zoom</span>
-      <Button variant="outlined"
+      <Button
+        variant="outlined"
         style={{
           marginBottom: '8px',
-          paddingLeft: '0px',
-          paddingRight: '0px',
         }}
+        onClick={handleZoomIn}
       >
         Zoom In
       </Button>
-      <Button variant="outlined">Zoom Out</Button>
+      <Button
+        variant="outlined"
+        onClick={handleZoomOut}
+      >Zoom Out</Button>
     </FormControl>
-
-  )
+  );
 }
