2d295b42675c9d1a1ea28476466e07ed1cd8960f
Ted Shaffer <shaffer.family@gmail.com>
2024-04-11 05:41:50 -0700

    Fixes for navigating through loupe view. Not 100% happy with it, as I don't understand why the other approach didn't work.

--- src/components/ViewSpec.tsx
+++ src/components/ViewSpec.tsx
@@ -7,23 +7,32 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import { TedTaggerDispatch, setPhotoLayoutRedux, } from '../models';
+import { TedTaggerDispatch, setLoupeViewMediaItemIdRedux, setPhotoLayoutRedux, } from '../models';
 import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
-import { getPhotoLayout } from '../selectors';
-import { PhotoLayout } from '../types';
+import { getMediaItems, getPhotoLayout, getSelectedMediaItemIds } from '../selectors';
+import { MediaItem, PhotoLayout } from '../types';
 
 import ZoomGroup from './ZoomGroup';
 
 export interface ViewSpecProps {
   photoLayout: PhotoLayout;
+  loupeViewMediaItemId: string;
   onSetPhotoLayout: (photoLayout: PhotoLayout) => void;
+  onSetLoupeViewMediaItemId: (id: string) => any;
+  onSetPhotoLayoutRedux: (photoLayout: PhotoLayout) => any;
 }
 
 const ViewSpec = (props: ViewSpecProps) => {
 
   function handleUpdatePhotoLayout(event: React.ChangeEvent<HTMLInputElement>, value: string): void {
     const photoLayout: PhotoLayout = value as PhotoLayout;
-    props.onSetPhotoLayout(photoLayout);
+
+    if (photoLayout === PhotoLayout.Loupe) {
+      props.onSetLoupeViewMediaItemId(props.loupeViewMediaItemId);
+      props.onSetPhotoLayoutRedux(PhotoLayout.Loupe);
+    } else {
+      props.onSetPhotoLayout(photoLayout);
+    }
   }
 
   const getZoomGroupJsx = (): JSX.Element | null => {
@@ -74,14 +83,30 @@ const ViewSpec = (props: ViewSpecProps) => {
 
 function mapStateToProps(state: any) {
 
+  let loupeViewMediaItemId: string = '';
+  const selectedMediaItemIds: string[] = getSelectedMediaItemIds(state);
+  if (selectedMediaItemIds.length > 0) {
+    loupeViewMediaItemId = selectedMediaItemIds[0];
+  } else {
+    const mediaItems: MediaItem[] = getMediaItems(state);
+    if (mediaItems.length === 0) {
+      loupeViewMediaItemId = '';
+    } else {
+      loupeViewMediaItemId = mediaItems[0].googleId;
+    }
+  }
+
   return {
     photoLayout: getPhotoLayout(state),
+    loupeViewMediaItemId,
   };
 }
 
 const mapDispatchToProps = (dispatch: TedTaggerDispatch) => {
   return bindActionCreators({
     onSetPhotoLayout: setPhotoLayoutRedux,
+    onSetLoupeViewMediaItemId: setLoupeViewMediaItemIdRedux,
+    onSetPhotoLayoutRedux: setPhotoLayoutRedux,
   }, dispatch);
 };
 
