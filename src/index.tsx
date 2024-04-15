621e5c91f8f84a976ab3cd0aa6331b14908a30b5
Ted Shaffer <shaffer.family@gmail.com>
2024-04-15 06:53:06 -0700

    Moved code to suppress react warnings into separate file.

--- src/index.tsx
+++ src/index.tsx
@@ -18,57 +18,6 @@ export const store = createStore(
     applyMiddleware(thunkMiddleware)
   ));
 
-const stringIncludesAny = (str: string, arr: string[]): boolean => {
-  return arr.some(item => str.includes(item));
-};
-
-const errorTypesToDisable = [
-  'Warning: Unknown event handler property',
-  'Warning: React does not recognize the',
-];
-
-const errorVariableNamesInErrorMessagesToDisable = [
-  'keywordsAsTree',
-  'keywordNodesByNodeId',
-  'onUpdateKeywordAssignedToSelectedMediaItems',
-  'selectedMediaItemIds',
-  'mapKeywordNodeIdToSelectedMediaItemIds',
-];
-
-// Save original console methods
-const originalConsoleError = console.error;
-const originalConsoleWarn = console.warn;
-
-// Override console methods
-console.error = (...args) => {
-
-  // Check if the error message is from React
-  if (args.some(arg => arg
-    && typeof arg === 'string'
-    // && arg.startsWith('Warning: React does not recognize the'))
-    && stringIncludesAny(arg, errorTypesToDisable))
-  ) {
-    if (args.some(arg => arg
-      && typeof arg === 'string'
-      && stringIncludesAny(arg, errorVariableNamesInErrorMessagesToDisable))
-    )
-      // Suppress specific React warnings
-      return;
-  }
-  // For other errors, log them normally
-  originalConsoleError.apply(console, args);
-};
-
-console.warn = (...args) => {
-  // Check if the warning message is from React
-  // if (args.some(arg => arg && typeof arg === 'string' && arg.includes('Warning:'))) {
-  //   // Suppress React warnings
-  //   return;
-  // }
-  // For other warnings, log them normally
-  originalConsoleWarn.apply(console, args);
-};
-
 const container = document.getElementById('content');
 const root = createRoot(container!);
 
