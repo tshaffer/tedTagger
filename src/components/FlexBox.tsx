af3733397a7b73326460eb799a43b45552acea66
Ted Shaffer <shaffer.family@gmail.com>
2024-05-09 06:22:25 -0700

    further experimentation

--- src/components/FlexBox.tsx
+++ src/components/FlexBox.tsx
@@ -12,11 +12,94 @@ export interface FlexBoxProps {
 
 const FlexBox = (props: FlexBoxProps) => {
 
+  const getRow0 = (): JSX.Element => {
+    return (
+      <div style={{
+        display: 'flex',
+        height: '240px'
+      }}>
+        <div style={{
+          flex: 100,
+          backgroundColor: 'lightgreen'
+        }}>
+          Item 1
+        </div>
+        <div style={{
+          flex: 200,
+          backgroundColor: 'lightblue'
+        }}>
+          Item 2
+        </div>
+      </div>
+    );
+  };
+
+  const getRow1 = (): JSX.Element => {
+    return (
+      <div style={{
+        display: 'flex',
+        height: '200px'
+      }}>
+        <div style={{
+          flex: 400,
+          backgroundColor: 'lightgreen'
+        }}>
+          Item 1
+        </div>
+        <div style={{
+          flex: 200,
+          backgroundColor: 'lightblue'
+        }}>
+          Item 2
+        </div>
+        <div style={{
+          flex: 300,
+          backgroundColor: 'navajowhite'
+        }}>
+          Item 2
+        </div>
+      </div>
+    );
+  };
+
+  const getRow3 = (): JSX.Element => {
+    return (
+      <div style={{
+        display: 'flex',
+        height: '500px'
+      }}>
+        <div style={{
+          flex: 600,
+          backgroundColor: 'lightgray'
+        }}>
+          Item 1
+        </div>
+        <div style={{
+          flex: 300,
+          backgroundColor: 'cyan'
+        }}>
+          Item 2
+        </div>
+        <div style={{
+          flex: 200,
+          backgroundColor: 'brown'
+        }}>
+          Item 2
+        </div>
+      </div>
+    );
+  };
+
+  const row0: JSX.Element = getRow0();
+  const row1: JSX.Element = getRow1();
+  const row3: JSX.Element = getRow3();
+
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: 1 }}>Item 1</div>
-      <div style={{ flex: 1 }}>Item 2</div>
-    </div>
+    <React.Fragment>
+      {row0}
+      {row1}
+      {row3}
+    </React.Fragment>
   );
 };
 
